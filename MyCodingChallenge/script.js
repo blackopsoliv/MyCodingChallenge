4782c187bd7132e5cd36f3eb587e6fed114e1d51
blackopsoliv <36663481+blackopsoliv@users.noreply.github.com>
2019-07-08 07:43:14 +0100

    Adding Test Cases For The Program

--- MyCodingChallenge/script.js
+++ MyCodingChallenge/script.js
@@ -1,5 +1,42 @@
 function main() {}
 
-function prepareTestCases() {}
+function prepareTestCases() {
+  var test_cases = [];
+  // happy-path cases
+  test_cases.push({
+    input: [""],
+    expectedOutput: [""]
+  });
+  test_cases.push({
+    input: ["a"],
+    expectedOutput: ["a"]
+  });
+  test_cases.push({
+    input: ["a", "b", "c"],
+    expectedOutput: ["a", "b", "c"]
+  });
+  test_cases.push({
+    input: ["a", "bc", "c"],
+    expectedOutput: ["a", "c", "b"]
+  });
+  test_cases.push({
+    input: ["a", "bc", "cf", "da", "eb", "f"],
+    expectedOutput: ["a", "d", "f", "c", "b", "e"]
+  });
+  test_cases.push({
+    input: ["ac", "bc", "c"],
+    expectedOutput: ["c", "b", "a"]
+  });
+  // error cases
+  test_cases.push({
+    input: ["a", "b", "cc"],
+    expectedOutput: []
+  });
+  test_cases.push({
+    input: ["a", "bc", "cf", "da", "e", "fb"],
+    expectedOutput: []
+  });
+  return test_cases;
+}
 
 function runTest() {}
